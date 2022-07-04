5789fcb96c0f5284ff59befd5c8ae2bb5e7d51e8
Pedro <pedro.souza@viacometa.com.br>
2022-07-04 14:47:02 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,210 +1,55 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    disable-sort
-    hide-default-footer
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Robôs</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              Adicionar Robô
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Nome"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.public_id"
-                      label="Public Id"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
-              >
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+  <v-app>
+    <v-main>
+      <v-toolbar>
+        <v-toolbar-title> Client Orchestrator </v-toolbar-title>
+        <v-btn> Adicionar </v-btn>
       </v-toolbar>
-    </template>
-    <template v-slot:[`item.actions`]="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
-    </template>
-  </v-data-table>
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th class="text-left">Nome</th>
+            <th class="text-left">ID Público</th>
+            <th class="text-left">Diretório</th>
+            <th class="text-left">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="robot in robots" :key="robot.name">
+            <td>{{ robot.name }}</td>
+            <td>{{ robot.publicId }}</td>
+            <td>{{ robot.directory }}</td>
+            <td>
+              <v-btn icon >
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-btn icon >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    headers: [
-      {
-        text: "Nome",
-        align: "start",
-        value: "name",
-      },
-      { text: "Public ID", value: "public_id" },
-      { text: "Directory", value: "directory" },
-      { text: "Actions", value: "actions", sortable: false },
-    ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      public_id: 0,
-      directory: "",
-    },
-    defaultItem: {
-      name: "",
-      public_id: 0,
-      directory: "",
-    },
-  }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    },
-  },
-
-  created() {
-    this.initialize();
-  },
-
-  methods: {
-    initialize() {
-      this.desserts = [
+  data() {
+    return {
+      robots: [
         {
-          name: "Peças 4 Rodas",
-          public_id: "1a2b3c",
-          directory: "dir",
+          name: "Cotas",
+          publicId: "Baixa CNH",
+          directory:
+            "C:/Users/Pedro Souza/Documents/ProjetosUiPath/PrimeiroProjetoUiPath",
         },
-        {
-          name: "Baixa CNH",
-          public_id: "1a2b3c",
-          directory: "dir",
-        },
-        {
-          name: "Notas Canceladas",
-          public_id: "1a2b3c",
-          directory: "dir",
-        },
-      ];
-    },
-
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
-      this.closeDelete();
-    },
-
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-      }
-      this.close();
-    },
+      ],
+    };
   },
 };
 </script>
+
+<style>
+</style>
